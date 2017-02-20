6e27d6f105bb8f310beb5050d54bb4c6703fbebc
John Spirk <stjfs03@moravian.edu>
2017-02-20 12:45:03 -0500

    implemented my class

--- SpirkConditionsDisplay.java
+++ SpirkConditionsDisplay.java
@@ -1,5 +1,25 @@
-/**
- * Created by JohnSpirk on 2/20/17.
- */
-public class SpirkConditionsDisplay {
+package HW6.observer;
+
+public class SpirkConditionsDisplay implements Observer, DisplayElement {
+
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
+
+    public SpirkConditionsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    public void display() {
+        System.out.println("Spirk Current conditions: " + temperature
+                + "F degrees and " + humidity + "% humidity");
+    }
+}
 }

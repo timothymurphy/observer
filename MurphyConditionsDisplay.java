e1ba3050bf7265609a003cda7ad7913d702dc033
murphyt <sttam09@moravian.edu>
2017-02-21 10:18:02 -0500

    Murphy Conditions Display added

---
+++ MurphyConditionsDisplay.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Peeke on 2/20/17.
+ */
+package HW6.observer;
+
+public class PeekeConditionsDisplay implements Observer, DisplayElement
+    {
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
+
+    public PeekeConditionsDisplay(Subject weatherData)
+    {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temperature, float humidity, float pressure)
+    {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    public void display()
+    {
+        System.out.println("Murphy Current conditions: " + temperature
+                + "F degrees and " + humidity + "% humidity");
+    }
+}
